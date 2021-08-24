7c4cab7acc5463f6d67d36fb3fd1c34054b1480e
abhiram11 <abhiramsatpute@gmail.com>
2021-08-24 17:06:30 +0530

    starting with header, installed styled components, material ui

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavbarContainer } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <h1>Myntra Logo</h1>
+      <h1>Men</h1>
+      <h1>Women</h1>
+      <h1>Kids</h1>
+      <h1>Home & Living</h1>
+      <h1>Beauty</h1>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
